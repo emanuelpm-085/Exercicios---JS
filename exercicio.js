b2cdadc3674272f577fa482c9dac58a0e33d890c
emanuelpm-085 <emanuelpm2025@gmail.com>
2025-12-03 21:27:11 -0300

    exercicios com JS

---
+++ exercicio.js
@@ -0,0 +1,73 @@
+//Exercicio em Lógica com JS
+// 1 string, number, booleano
+const nome = "Emanuel";
+
+console.log(nome);
+console.log(typeof nome);
+
+//number
+
+const numero1 = 20;
+const numero2 = 20;
+
+const soma = numero1 +numero2;
+
+console.log(soma);
+
+//booleano
+
+const aprovado = true;
+console.log("O aluno está aprovado?");
+console.log(aprovado);
+
+//variavel com ',"", template strings
+
+const texto = 'Emanuel';
+
+console.log(texto);
+
+const name = "Emanuel";
+
+console.log(name);
+
+//template strings
+
+const txt = "Emanuel";
+const mensagem = "mota";
+
+console.log(`Olá! ${txt} ${mensagem}`)
+
+//variavel usando let
+
+let idade = 20;
+
+console.log(idade);
+
+idade = 25
+
+console.log(idade);
+
+//alterar o valor de uma variavel
+
+// const numero = 30;
+
+// numero = 35
+// o erro apresentado e, atribuir novos valores a variaveis constantes.
+
+let numero3 = 10;
+let numero4 = 10;
+let adicao = numero3 + numero4;
+let resultAdicao = adicao - numero4;
+let resulSubtraçao = resultAdicao * numero4;
+let result = 50 / 10;
+let rest = 50 % 10;
+
+console.log(adicao);
+console.log(resultAdicao);
+console.log(resulSubtraçao);
+console.log(result);
+console.log(rest)
+
+
+
+
