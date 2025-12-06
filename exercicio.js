4d322ea2ebd56d9a0fe3a6e6ea23ccbc26b3b6b6
emanuelpm-085 <emanuelpm2025@gmail.com>
2025-12-06 12:44:29 -0300

    exercicios finalizados

--- exercicio.js
+++ exercicio.js
@@ -70,7 +70,6 @@ console.log(resulSubtraçao);
 console.log(result);
 console.log(rest);
 
-
 //variavel nota
 
 const nota = 8;
@@ -113,7 +112,6 @@ console.log(sobreNome [4]);
 
 sobreNome[3] = "Ednardo";
 
-
 console.log(sobreNome)
 
 // array com  numeros
@@ -124,5 +122,79 @@ const add = numeros[0] + numeros[1] + numeros[2];
 
 console.log(add)
 
+// objeto com nome,idade,e cidade
 
+const pessoa ={
+    nome:"Emanuel",
+    idade: 18,
+    cidade: "fortaleza",
+};
+console.log(pessoa);
+console.log(pessoa.nome);
+console.log(pessoa.idade);
+console.log(pessoa.cidade);
 
+// adicione profissao
+
+pessoa.profissao = "programador"
+console.log(pessoa);
+
+// delete a propiedade idade
+
+delete pessoa.idade;
+console.log(pessoa);
+
+// objeto representando produto
+
+const produto ={
+    nome: "Civic",
+    preço: 18.000,
+};
+
+console.log(typeof produto.nome);
+console.log(typeof produto.preço);
+
+// array com alunos
+
+const alunos =[
+    {nome: "Manel", nota: 8},
+    {nome: "Mota", nota: 7},
+    {nome: "Pontes", nota: 10},
+];
+
+console.log(`A nota do segundo aluno é: ${alunos[1].nota}`);
+
+// array com cada tipo (string, booleano, number)
+
+const array = "manel";
+const array2 = 18;
+const temMoto = false; 
+
+console.log(typeof array);
+console.log(typeof array2);
+console.log(typeof temMoto);
+
+// template strings para exibir frase
+
+const nome1 = "Emanuel";
+const idade2 = 15;
+
+console.log(`Meu nome é ${nome1} e tenho ${idade2} anos`);
+
+// somando o saldo
+
+let saldo = 100; 
+let gasto = 30;
+
+saldo = saldo - gasto;
+
+console.log(`Seu saldo atualizado é: ${saldo}`);
+
+// senhaCorreta e senhaDigitando
+
+const senhaCorreta = "1234";
+const senhaDigitada = "1234";
+
+const senhaEstaCorreta = senhaDigitada === senhaCorreta;
+
+console.log(`A senha está correta? ${senhaEstaCorreta}`);
